4a634aecd7072afafa029d609873b2c00adafabe
Shivam Kashyap <shivamkumarkashyap12@gmail.com>
2023-01-22 01:09:36 +0530

    All Question done, Error in StringNumAddition

---
+++ IntegerToRoman.java
@@ -0,0 +1,21 @@
+public class IntegerToRoman {
+    public String IntToRoman(int num) {
+        int[] intCode = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String[] code = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+        String sb = "";
+        for (int i = 0; i < intCode.length; i++) {
+            while (num >= intCode[i]) {
+                sb = sb + code[i];
+                num -= intCode[i];
+            }
+        }
+        return sb;
+    }
+
+    public static void main(String[] args) {
+        IntegerToRoman obj = new IntegerToRoman();
+
+        System.out.println(obj.IntToRoman(192));
+    }
+}
